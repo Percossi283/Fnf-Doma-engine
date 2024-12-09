8b472167e1be0190e7a9195c91188978cd44e72d
Percossi283 <eros.percossi@gmail.com>
2024-12-09 17:23:59 +0100

    Initial commit

---
+++ source/FlashingState.hx
@@ -0,0 +1,72 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+class FlashingState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Welcome to Psych Engine 0.5.3 on Android!\n
+			We tried to add Shaders and we failed :'D\n
+			But seriously tho, we recommend you to disable Flashing Lights \n
+			if you're Photosensitive \n
+			Press A to disable them or\n
+			Press B to ignore this message.",
+			32);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+
+		#if android
+		addVirtualPad(NONE, A_B);
+		#end
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			var back:Bool = controls.BACK;
+			if (controls.ACCEPT || back) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				if(!back) {
+					ClientPrefs.flashing = false;
+					ClientPrefs.saveSettings();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							MusicBeatState.switchState(new TitleState());
+						});
+					});
+				} else {
+					FlxG.sound.play(Paths.sound('cancelMenu'));
+					FlxTween.tween(warnText, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
+					});
+				}
+			}
+		}
+		super.update(elapsed);
+	}
+}
